22b3e2822fc76a73ed570a2978ccaa4a4c8af901
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2023-05-09 15:42:29 +0200

    ok

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
